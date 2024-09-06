61cf912aa346e52e7f5daf0a2bd04d5e8147a9cc
maltalucas7 <flucasofficial@gmail.com>
2024-09-05 22:56:07 -0300

    atualizacao #4

--- apiVotting/settings.py
+++ apiVotting/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'apiVotting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',  # Substitua pelo nome real do seu banco de dados
         'USER': 'root',        # Substitua pelo seu nome de usuário MySQL
         'PASSWORD': 'RUiFcxbBhGwfmtRRDuJRIYdwFBukLLaO',      # Substitua pela sua senha MySQL
