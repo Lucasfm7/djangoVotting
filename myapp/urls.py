4210488aeeda8d8167dfcb70ba6f9dcaf7006759
maltalucas7 <flucasofficial@gmail.com>
2024-09-15 23:29:23 -0300

    atualizacao #30

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PessoaViewSet, PesquisarPessoaPorCPFAPIView
+from .views import PessoaViewSet
 
 # Cria o roteador para as URLs
 router = DefaultRouter()
 router.register(r'pessoas', PessoaViewSet)
 
 urlpatterns = [
-    # path('send-sms/', SendSMSAPIView.as_view(), name='send-sms'),
-    path('pesquisar-pessoa-cpf/', PesquisarPessoaPorCPFAPIView.as_view(), name='pesquisar-pessoa-cpf'),
     path('', include(router.urls)),  # Inclui as rotas geradas pelo ViewSet
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets
-from rest_framework.views import APIView
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.views import APIView
 from .models import Pessoa
 from .serializers import PessoaSerializer
 
@@ -25,34 +25,13 @@ class PessoaViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'])
     def pesquisar_cpf(self, request):
         cpf = request.query_params.get('cpf', None)
-        if cpf:
-            pessoa = Pessoa.objects.filter(cpf=cpf).first()
-            if pessoa:
-                serializer = PessoaSerializer(pessoa)
-                return Response(serializer.data)
-            return Response({"detail": "CPF não encontrado."}, status=404)
-        return Response({"detail": "CPF não fornecido."}, status=400)
-
-class PesquisarPessoaPorCPFAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        # Obtém o CPF dos parâmetros da URL
-        cpf = request.query_params.get('cpf')
-
         if not cpf:
-            return Response(
-                {"error": "O campo 'cpf' é obrigatório."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({"detail": "CPF não fornecido."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Pesquisa a pessoa com o CPF fornecido
         pessoa = Pessoa.objects.filter(cpf=cpf).first()
-
         if pessoa:
-            # Serializa a instância da Pessoa encontrada
             serializer = PessoaSerializer(pessoa)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                {"error": "Pessoa não encontrada."},
-                status=status.HTTP_404_NOT_FOUND
-            )
+        
+        return Response({"detail": "CPF não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+
