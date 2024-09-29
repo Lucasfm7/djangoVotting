ec8977a3cd4b32ff24a170fa02b2f89b93a47dc7
maltalucas7 <flucasofficial@gmail.com>
2024-09-29 15:21:33 -0300

    atualizacao #38

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,8 @@
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.hashers import make_password, check_password
+
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=255)
@@ -39,3 +41,17 @@ class VerificationCode(models.Model):
 
     def __str__(self):
         return f"VerificationCode(phone_number={self.phone_number}, code={self.code})"
+
+class AdminUser(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)  # Armazena a senha com hash
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self.save()
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return self.username
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PessoaViewSet, CandidateViewSet, VoteViewSet, SendSMSView, SendVerificationCodeView, VerifyCodeView
+from .views import PessoaViewSet, CandidateViewSet, VoteViewSet, SendSMSView, SendVerificationCodeView, VerifyCodeView, AdminLoginView
 
 router = DefaultRouter()
 router.register(r'pessoas', PessoaViewSet, basename='pessoa')
@@ -14,4 +14,5 @@ urlpatterns = [
     path('enviar_sms/', SendSMSView.as_view(), name='enviar_sms'),
     path('send_verification_code/', SendVerificationCodeView.as_view(), name='send_verification_code'),
     path('verify_code/', VerifyCodeView.as_view(), name='verify_code'),
+    path('admin/login/', AdminLoginView.as_view(), name='admin_login'),
 ]
